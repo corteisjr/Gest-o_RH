7a13d1cbee468ccad3f1e78db7ed524a066df020
corteisjr <corteisjunior@gmail.com>
2022-01-08 10:08:52 +0200

    crud empresa parte 2

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from empresas.models import Empresa
@@ -5,3 +6,10 @@ from empresas.models import Empresa
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['name']
+    
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresas = obj
+        funcionario.save()
+        return HttpResponse('Ok')
