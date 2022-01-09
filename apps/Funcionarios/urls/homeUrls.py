ac82c8f5fb81bad66f5f6f4cf813e1ee78698e01
corteisjr <corteisjunior@gmail.com>
2022-01-09 08:52:58 +0200

    Crud Funcionario

--- apps/Funcionarios/urls/homeUrls.py
+++ apps/Funcionarios/urls/homeUrls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from apps.Funcionarios.views import funcionariosView
+from apps.Funcionarios.views import listFuncionario
 
 urlpatterns = [
-    path('', funcionariosView, name='funcionarios')
+    path(' ', listFuncionario.as_view(), name='list_funcionarios'),
 ]
--- apps/Funcionarios/views.py
+++ apps/Funcionarios/views.py
@@ -1,5 +1,8 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from  Funcionarios.models import Funcionario
 
-def funcionariosView(request):
-    return HttpResponse('<h1>Home Page</h1>')
+class listFuncionario(ListView):
+    model = Funcionario
+    
