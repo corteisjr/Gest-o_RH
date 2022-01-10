ce63e8f72b5c57baefdcc35bd231c3302f3344ba
corteisjr <corteisjunior@gmail.com>
2022-01-10 08:51:40 +0200

    Filtração pela empresa logada!

--- apps/Funcionarios/views.py
+++ apps/Funcionarios/views.py
@@ -4,5 +4,12 @@ from django.urls import reverse
 from django.views.generic import ListView
 from  Funcionarios.models import Funcionario
 
+
 class listFuncionario(ListView):
     model = Funcionario
+
+    # Filtração pela empresa logada
+    def get_queryset(self):
+        empresa_logada= self.request.user.funcionario.empresas
+        queryset= Funcionario.objects.filter(empresas=empresa_logada)
+        return queryset
