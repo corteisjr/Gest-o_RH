0ab2231d52573c46a5c9c3c689e8f2f1d499ee5e
corteisjr <corteisjunior@gmail.com>
2022-01-13 13:57:33 +0200

    Delete funcionário

--- apps/Funcionarios/views.py
+++ apps/Funcionarios/views.py
@@ -1,10 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.urls.base import reverse_lazy
 from django.views.generic import ListView, UpdateView
+from django.views.generic.edit import DeleteView
 from  Funcionarios.models import Funcionario
 
 
+# Listar
 class listFuncionario(ListView):
     model = Funcionario
 
@@ -13,7 +16,14 @@ class listFuncionario(ListView):
         empresa_logada= self.request.user.funcionario.empresas
         queryset= Funcionario.objects.filter(empresas=empresa_logada)
         return queryset
-    
+
+# Actualizar
 class updateFuncionario(UpdateView):
     model = Funcionario
     fields = ['nome', 'departamentos']
+    
+# Deletar
+
+class deleteFuncionario(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
