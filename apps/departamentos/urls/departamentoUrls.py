a2c2e6d75d24583ec7d08b04d3456b60e19b262e
corteisjr <corteisjunior@gmail.com>
2022-01-16 20:09:19 +0200

    complet Crud for departamentos

--- apps/departamentos/urls/departamentoUrls.py
+++ apps/departamentos/urls/departamentoUrls.py
@@ -1,8 +1,10 @@
 from unicodedata import name
 from django.urls import path
-from departamentos.views import listDepartamento, updadeDepartamento
+from departamentos.views import DeleteDepartamento, listDepartamento, updadeDepartamento, CreateDepartamento
 
 urlpatterns = [
     path('list', listDepartamento.as_view(), name='list_departamentos'),
-    path('edit/<int:pk>/', updadeDepartamento.as_view(), name='update_departamentos')
+    path('novo/', CreateDepartamento.as_view(), name='new_departamentos'),
+    path('edit/<int:pk>/', updadeDepartamento.as_view(), name='update_departamentos'),
+    path('delete/<int:pk>/', DeleteDepartamento.as_view(), name='delete_departamentos')
 ]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import redirect, render
+from django.urls.base import reverse_lazy
 from django.views.generic import ListView, UpdateView
+from django.contrib.auth.models import User
+from django.views.generic.edit import DeleteView, CreateView
 from departamentos.models import Departamento
 # Listar Departamentos
 
@@ -11,7 +14,23 @@ class listDepartamento(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresas
         return Departamento.objects.filter(empresas=empresa_logada)
-    
+
+#Actualizar
 class updadeDepartamento(UpdateView):
     model = Departamento
     fields = ['nome']
+    
+class CreateDepartamento(CreateView):
+    model = Departamento
+    fields = ['nome']
+    
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresas = self.request.user.funcionario.empresas
+        departamento.save()
+        return super(CreateDepartamento, self).form_valid(form)
+        
+#Deletar
+class DeleteDepartamento(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
