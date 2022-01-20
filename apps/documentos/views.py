63e24cb46b00fea8b8a65f2f5d536446a4c0316b
corteisjr <corteisjunior@gmail.com>
2022-01-20 09:55:27 +0200

    bug fixed

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
-from django.urls.base import reverse_lazy
-from django.views.generic.edit import CreateView, UpdateView
-from .models import Documento
+# from django.shortcuts import render
+# from django.urls.base import reverse_lazy
+# from django.views.generic.edit import CreateView, UpdateView
+# from .models import Documento
 
-class DocumentCreate(CreateView):
-    model = Documento
-    fields = ['descricao', 'arquivo']
+# class DocumentCreate(CreateView):
+#     model = Documento
+#     fields = ['descricao', 'arquivo']
     
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        form.instance.funcionario_id = self.kwargs['funcionarios_id']
+#     def post(self, request, *args, **kwargs):
+#         form = self.get_form()
+#         form.instance.funcionario_id = self.kwargs['funcionarios_id']
         
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+#         if form.is_valid():
+#             return self.form_valid(form)
+#         else:
+#             return self.form_invalid(form)
